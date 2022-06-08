4169de529079633e2dfda0a44903aea0d18e5b25
Julius Odunuga <julius4sam@gmail.com>
2022-06-08 14:21:50 +0000

    pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,13 +4,13 @@ def roman_to_int(roman_string):
     if (type(roman_string) != str) or len(roman_string) < 1:
         return 0
     roman = {
-        "I" : 1,
-        "V" : 5,
-        'X' : 10,
-        "L" : 50,
-        "C" : 100, 
-        "D" : 500,
-        "M" : 1000
+        "I": 1,
+        "V": 5,
+        'X': 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
     number = 0
     prev = 10000
