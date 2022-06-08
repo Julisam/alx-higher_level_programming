9edcd282ced0ae73b8cdebf44ca91c0da777ab05
Julius Odunuga <julius4sam@gmail.com>
2022-06-08 15:41:08 +0000

    pycodestyles

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,6 @@
 def weight_average(my_list=[]):
     if my_list is None or len(my_list) < 1:
         return 0
-    prod = sum([a*b for a,b in my_list])
-    sumw = sum([b for a,b in my_list])
+    prod = sum([a * b for a, b in my_list])
+    sumw = sum([b for a, b in my_list])
     return prod / sumw
