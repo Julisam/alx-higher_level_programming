79bbf8d39ceaf16b445ec570b32991f4aca16ddd
Julius Odunuga <julius4sam@gmail.com>
2022-06-08 12:44:40 +0000

    error in func name

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-def uniq_add(my_list=[]):
-    return [[num**2 for num in row] for row in my_list]
+def square_matrix_simple(matrix=[]):
+    return [[num**2 for num in row] for row in matrix]
